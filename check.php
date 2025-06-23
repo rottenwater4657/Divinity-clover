e8e49c1797464fc873f3f4ca4550d513336370fa
rottenwater <nirgun.bhujel100@gmail.com>
2025-06-23 13:46:18 +0545

    final

--- check.php
+++ check.php
@@ -1,27 +1,38 @@
-
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+    $admin_code = $_POST['admin_code'];
+    if ($admin_code === "1234") { 
+        header("Location: staffsignup.php");
+        exit();
+    } else {
+        echo "<p>Access denied.</p>";
+    }
+}
+?> 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Check</title>
+     <link rel="stylesheet" href="check.css">
+    <link rel="icon" href="images/logo.png" type="image/png">
 </head>
 <body>
+    <div class="video-container">
+        <video loop autoplay muted id="background-video">
+            <source src="videos/check.mp4" type="video/mp4">
+        </video>
+    </div>
+    <div class="form-container">
 <h1>Enter Admin Code</h1>
-<form action="" method="post">
+<form  method="post">
     <input type="text" name="admin_code" placeholder="Admin Code" required>
     <button type="submit">Check</button>
 </form>
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $admin_code = $_POST['admin_code'];
-    if ($admin_code === "1234") {
-        header("refresh:1.5;url=staffsignup.php");
-            exit();
-    } else {
-        echo "<p>Access denied.</p>";
-    }
-}
-?>
+</div>
+  
 </body>
 </html>
+       
+
