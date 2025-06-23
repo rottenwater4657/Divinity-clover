60231b7599decafcacc8d069e4d23df619e4c283
shrijan <shrijan8shrestha@gmail.com>
2025-06-23 08:33:16 +0545

    slogin

--- slogin.php
+++ slogin.php
@@ -1,4 +1,42 @@
+<?php
+session_start();
+include("./sql.php");
+$conn=conntodb();
+if(isset($_POST['login'])){
+      $userName=$_POST['username'];
+    $password=$_POST['password'];
+    $sql="select * from staffs where username=?";
+     $stmt = mysqli_prepare($conn, $sql);
+     if ($stmt) {
 
+        mysqli_stmt_bind_param($stmt, "s", $userName);
+
+        mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    if ($result && mysqli_num_rows($result) === 1) {
+        $row = mysqli_fetch_assoc($result);
+
+        if (password_verify($password, $row['hash_password'])) {
+           echo "loading...";
+           $_SESSION['user_id'] = $row['id'];
+            header("refresh:1.5;url=../apage/adminpage.php");
+            exit();
+        } else {
+            echo "Wrong Username Or Password";
+        }
+    }
+    mysqli_stmt_close($stmt);
+    } else {
+        echo "Query preparation failed.";
+    }
+
+
+
+}
+
+
+?>
 <!DOCTYPE html> <html lang="en"> 
     <head> <meta charset="UTF-8"> 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
